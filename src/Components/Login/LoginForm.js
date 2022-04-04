6d6cee7ea21a49ffd4b231a3f5478e058cdd2709
RodrigoFonsecaG <rodrigofon1@hotmail.com>
2022-04-04 08:14:18 -0300

    Login form

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -4,6 +4,9 @@ import Input from '../Form/Input';
 import Button from '../Form/Button';
 import useForm from '../../Hooks/useForm';
 import { UserContext } from '../../UserContext';
+import Error from '../Helper/Error';
+import styles from './LoginForm.module.css';
+import stylesBtn from '../Form/Button.module.css';
 
 
 
@@ -11,7 +14,7 @@ const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  const { userLogin } = React.useContext(UserContext);
+  const { userLogin, error, loading } = React.useContext(UserContext);
 
 
   async function handleSubmit(event) {
@@ -22,14 +25,22 @@ const LoginForm = () => {
   }
 
   return (
-    <section>
-      <h1>Login</h1>
-      <form action="" onSubmit={handleSubmit}>
+    <section className="animeLeft">
+      <h1 className="title">Login</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
-        <Button>Entrar</Button>
+        {loading ? <Button disabled>Carregando...</Button> : <Button>Entrar</Button>}
+        <Error error={error}/>
       </form>
-      <Link to="/login/criar">Cadastro</Link>
+      <Link className={styles.perdeu} to="login/perdeu">Perdeu a senha?</Link>
+
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Ainda não possui conta? Cadastre-se no site</p>
+      <Link to="/login/criar" className={stylesBtn.button}>Cadastro</Link>
+      </div>
+
     </section>
   );
 };
--- src/UserContext.js
+++ src/UserContext.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { USER_GET, TOKEN_POST } from './api';
+import { USER_GET, TOKEN_POST, VALIDATE_TOKEN_POST } from './api';
+import { useNavigate } from 'react-router-dom';
 
 export const UserContext = React.createContext();
 
@@ -9,33 +10,93 @@ export const UserStorage = ({ children }) => {
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState(null);
 
+  const navigate = useNavigate();
+
+  // Resetamos tudo caso haja logout
+  const userLogout = React.useCallback(
+    async function () {
+      setData(null);
+      setError(null);
+      setLoading(false);
+      setLogin(false);
+
+      window.localStorage.removeItem('token');
+
+      navigate('/login');
+    },
+    [navigate]
+  );
+
   async function getUser(token) {
     const { url, options } = USER_GET(token);
 
-      const response = await fetch(url, options);
-      const json = await response.json();
+    const response = await fetch(url, options);
+    const json = await response.json();
 
-      setData(json);
-      setLogin(true);
-      console.log(json)
+    setData(json);
+    setLogin(true);
+    console.log(json);
   }
 
   async function userLogin(username, password) {
-    const { url, options } = TOKEN_POST({ username, password });
+    try {
+      setError(null);
+      setLoading(true);
 
-    const tokenRes = await fetch(url, options);
-    const { token } = await tokenRes.json();
-    window.localStorage.setItem('token', token);
-    getUser(token);
+      const { url, options } = TOKEN_POST({ username, password });
+
+      const tokenRes = await fetch(url, options);
+      if (!tokenRes.ok) throw new Error(`Error: Usuário Inválido!`);
+
+      const { token } = await tokenRes.json();
+      window.localStorage.setItem('token', token);
+      await getUser(token);
+      navigate('/conta');
+    } catch (error) {
+      setError(error.message);
+      setLogin(false);
+    } finally {
+      setLoading(false);
+    }
   }
+
+  // Loga automaticamente se ja existir um token no localStorage
+  React.useEffect(() => {
+    async function autoLogin() {
+      const token = localStorage.getItem('token');
+
+      if (token) {
+        try {
+          setError(null);
+          setLoading(true);
+
+          //Valida o token
+          const { url, options } = VALIDATE_TOKEN_POST(token);
+          const response = await fetch(url, options);
+
+          // Se o token for invalido cria um erro
+          if (!response.ok) throw new Error('Token inválido');
+
+          // Se tudo der certo loga o usuario
+          await getUser(token);
+        } catch (error) {
+          userLogout();
+        } finally {
+          setLoading(false);
+        }
+      }
+    }
+
+    autoLogin();
+  }, [userLogout]);
 
   return (
     <div>
-      <UserContext.Provider value={{userLogin, data}}>
+      <UserContext.Provider
+        value={{ userLogin, userLogout, data, error, loading, login }}
+      >
         {children}
       </UserContext.Provider>
     </div>
   );
 };
-
-
