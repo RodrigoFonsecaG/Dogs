a61e7834f15526c9483ce6564afff8ced9d87b0a
RodrigoFonsecaG <rodrigofon1@hotmail.com>
2022-04-04 10:34:21 -0300

    User Header

--- src/Components/Helper/ProtectedRoute.js
+++ src/Components/Helper/ProtectedRoute.js
@@ -5,6 +5,8 @@ import {UserContext} from '../../UserContext'
 const ProtectedRoute = ({children}) => {
     const { login } = React.useContext(UserContext);
 
+    console.log(login)
+
     // Se tiver login retorna o nosso conteudo, se não navega para /login
     return login ? <div>{children}</div> : <Navigate to="/login"/>
 }
