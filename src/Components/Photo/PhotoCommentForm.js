20654146fc8304f1f447d2c1fbb51a4989940d6e
RodrigoFonsecaG <rodrigofon1@hotmail.com>
2022-04-06 18:50:07 -0300

    Photos comments

---
+++ src/Components/Photo/PhotoCommentForm.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { COMMENT_POST } from '../../Api';
+import { ReactComponent as Enviar } from '../../Assets/enviar.svg';
+import Error from '../Helper/Error.js'
+import useFetch from '../../Hooks/useFetch';
+import styles from './PhotoCommentForm.module.css';
+
+const PhotoCommentForm = ({ id, setPostComments }) => {
+  const [comment, setComment] = React.useState('');
+  const { request, error } = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = COMMENT_POST(id, { comment });
+    const { response, json } = await request(url, options);
+
+    if (response.ok) {
+      setComment('');
+
+      //Enviamos json ja que o comentario é um objeto
+      setPostComments((comments) => [...comments, json]);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <textarea
+        className={styles.textarea}
+        value={comment}
+        onChange={({ target }) => setComment(target.value)}
+        name="comment"
+        id="comment"
+        placeholder="Comente..."
+      />
+
+      <button className={styles.button}>
+        <Enviar />
+      </button>
+      {error && <Error error={error}/>}
+    </form>
+  );
+};
+
+export default PhotoCommentForm;
--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { UserContext } from '../../UserContext';
+import PhotoCommentForm from './PhotoCommentForm';
+import styles from './PhotoComments.module.css';
 
-const PhotoComments = () => {
+const PhotoComments = ({ id, comments }) => {
+  // Formulario só aparece se nosso login do nosso contexto for true.
+  const { login } = React.useContext(UserContext);
+
+  //Passamos um callback que roda uma vez e defini o estado inicial, nesse caso os comentarios da foto que estamos recebendo como propriedade
+  const [postComments, setPostComments] = React.useState(() => comments);
+
+  const commentsSection = React.useRef(null);
+
+  React.useEffect(() => {
+    commentsSection.current.scrollTop = commentsSection.current.scrollHeight;
+  }, [comments]);
+
   return (
-    <div>PhotoComments</div>
-  )
-}
+    <>
+      <ul ref={commentsSection} className={styles.comments}>
+        {postComments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentForm id={id} setPostComments={setPostComments} />}
+    </>
+  );
+};
 
-export default PhotoComments
+export default PhotoComments;
