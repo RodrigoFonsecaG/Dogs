b34b0b1d5775567757870242abe5ae995431382b
RodrigoFonsecaG <rodrigofon1@hotmail.com>
2022-04-06 08:47:37 -0300

    photo modal

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -3,12 +3,21 @@ import FeedModal from './FeedModal'
 import FeedPhotos from './FeedPhotos';
 
 const Feed = () => {
+  const [modalPhoto, setModalPhoto] = React.useState(null);
+
+  console.log(modalPhoto);
+  
   return (
     <div>
-      <FeedModal/>
-      <FeedPhotos/>
+      {modalPhoto && (
+        <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />
+      )}
+      <FeedPhotos setModalPhoto={setModalPhoto} />
     </div>
-  )
+  );
 }
 
 export default Feed
+
+
+
